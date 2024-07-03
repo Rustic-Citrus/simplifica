4b1de1180bd04a8809617d6e4ac66ddfac62dba5
Rustic-Citrus <mrcurtis.english@gmail.com>
2024-07-03 12:53:57 +0100

    test: Header component

--- tests/Header.test.jsx
+++ tests/Header.test.jsx
@@ -1,14 +1,19 @@
-import { describe, it, expect } from "vitest";
 import { Header } from "../src/components/Header";
+import { AuthProvider } from "../src/hooks/useAuth";
+
+import { describe, it, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
-import { MemoryRouter } from "react-router-dom";
 import "@testing-library/jest-dom";
+
+import { MemoryRouter } from "react-router-dom";
 
 describe("Header component tests", () => {
   it("renders correctly", async () => {
     render(
       <MemoryRouter>
-        <Header />
+        <AuthProvider>
+          <Header />
+        </AuthProvider>
       </MemoryRouter>
     );
 
