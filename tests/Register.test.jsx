b3ad892a105f60b63b2262d4f2edc88e84430895
Rustic-Citrus <mrcurtis.english@gmail.com>
2024-07-03 12:55:48 +0100

    test: Register component

--- tests/Register.test.jsx
+++ tests/Register.test.jsx
@@ -1,5 +1,6 @@
 import { describe, it, expect } from "vitest";
 import { Register } from "../src/components/Register";
+import { AuthProvider } from "../src/hooks/useAuth";
 import { render, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import "@testing-library/jest-dom";
@@ -8,15 +9,25 @@ describe("Register component tests", () => {
   it("renders correctly", async () => {
     render(
       <MemoryRouter>
-        <Register />
+        <AuthProvider>
+          <Register />
+        </AuthProvider>
       </MemoryRouter>
     );
 
-    expect(await screen.findByLabelText("title")).toHaveTextContent("Create an account");
-    expect(await screen.findByLabelText("subtitle")).toHaveTextContent("Enter a username and password");
-    expect(await screen.findByLabelText("get-started-button")).toBeInTheDocument();
+    expect(await screen.findByLabelText("title")).toHaveTextContent(
+      "Create an account"
+    );
+    expect(await screen.findByLabelText("subtitle")).toHaveTextContent(
+      "Enter a username and password"
+    );
+    expect(
+      await screen.findByLabelText("get-started-button")
+    ).toBeInTheDocument();
     expect(await screen.findByLabelText("password-input")).toBeInTheDocument();
     expect(await screen.findByLabelText("username-input")).toBeInTheDocument();
-    expect(await screen.findByLabelText("confirm-password-input")).toBeInTheDocument();
+    expect(
+      await screen.findByLabelText("confirm-password-input")
+    ).toBeInTheDocument();
   });
 });
