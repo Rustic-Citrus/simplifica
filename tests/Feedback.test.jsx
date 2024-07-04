84a487b030483fd283706b6faa69381a8d75f447
Rustic-Citrus <mrcurtis.english@gmail.com>
2024-07-04 10:32:20 +0100

    test: Feedback component

---
+++ tests/Feedback.test.jsx
@@ -0,0 +1,20 @@
+import { afterEach, beforeEach, beforeAll, afterAll, expect, describe, it, vi } from "vitest";
+import "@testing-library/jest-dom/vitest";
+import { cleanup, render, screen } from "@testing-library/react";
+import { Feedback } from "../src/components/Feedback";
+
+describe("Feedback component tests", () => {
+  const testTitle = "Test";
+  const testMessage = "This is a test";
+  const testType = "success";
+  const mockToggleVisible = vi.fn();
+
+
+  it("renders correctly", async () => {
+    render(<Feedback title={testTitle} message={testMessage} type={testType} toggleVisible={mockToggleVisible} />);
+
+    expect(await screen.findByLabelText("feedback-title")).toBeInTheDocument();
+    expect(await screen.findByLabelText("feedback-timestamp")).toBeInTheDocument();
+    expect(await screen.findByLabelText("feedback-message")).toBeInTheDocument();
+  });
+});
