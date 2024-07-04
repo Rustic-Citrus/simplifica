457def8b0aea90e07028599f07bb7c70bbd4510f
Rustic-Citrus <mrcurtis.english@gmail.com>
2024-07-04 15:45:31 +0100

    test: LessonPlanService class

--- tests/LessonPlanService.test.js
+++ tests/LessonPlanService.test.js
@@ -28,6 +28,7 @@ describe("LessonPlanService class tests", () => {
   let validBaseUrl = "https://www.valid.com/url";
   let testUserId = "81742460220";
   let testLessonPlanService;
+  let testInstance;
 
   it("initialises the LessonPlanService class correctly", () => {
     const axiosCreateSpy = vi.spyOn(axios, "create");
@@ -38,4 +39,21 @@ describe("LessonPlanService class tests", () => {
     expect(testLessonPlanService.getUserId()).toEqual(testUserId);
     expect(testLessonPlanService.getInstance()).toHaveProperty("get");
   });
+
+  afterEach(() => {
+    testLessonPlanService = null;
+  })
+
+  beforeEach(() => {
+    testLessonPlanService = new LessonPlanService(validBaseUrl, testUserId);
+    testInstance = testLessonPlanService.getInstance();
+  });
+
+  it("returns a response when getLessonPlans is called", async () => {
+    const getSpy = vi.spyOn(testInstance, "get")
+
+    await testLessonPlanService.getLessonPlans();
+
+    expect(getSpy).toHaveBeenCalledWith(`/${testUserId}/lesson-plans`);
+  })
 });
