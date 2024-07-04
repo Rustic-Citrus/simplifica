f4742914aabda7597640290bad2c7f0e1d18236b
Rustic-Citrus <mrcurtis.english@gmail.com>
2024-07-04 11:05:34 +0100

    test: LessonPlanCreate component

--- tests/LessonPlanCreate.test.jsx
+++ tests/LessonPlanCreate.test.jsx
@@ -1,24 +1,60 @@
-import { afterEach, beforeEach, beforeAll, afterAll, expect, describe, it, vi } from "vitest";
+import {
+  expect,
+  describe,
+  it,
+  vi,
+} from "vitest";
 import "@testing-library/jest-dom/vitest";
-import { cleanup, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 
 import { LessonPlanCreate } from "../src/components/LessonPlanCreate";
-import { AuthProvider } from "../src/hooks/useAuth";
-import { MemoryRouter } from "react-router-dom";
-import "@testing-library/jest-dom";
+
+import * as useAuthModule from "../src/hooks/useAuth";
+
+vi.mock("../src/hooks/useAuth.jsx", () => {
+  const actual = vi.importActual("../src/hooks/useAuth.jsx");
+  return {
+    ...actual,
+    useAuth: vi.fn(),
+    AuthProvider: ({ children }) => children,
+  };
+});
+
+vi.mock("react-router-dom", () => {
+  const originalModule = vi.importActual("react-router-dom");
+  return {
+    ...originalModule,
+    useParams: () => ({
+      userId: "123",
+    }),
+    useNavigate: () => vi.fn(),
+    Link: ({ children, to }) => <a href={to}>{children}</a>,
+    BrowserRouter: originalModule.BrowserRouter,
+  };
+});
 
 describe("LessonPlanCreate component tests", () => {
-  it("renders correctly", async () => {
+  it("renders correctly when the valid user tries to create a lesson plan", async () => {
     const mockTriggerFeedback = vi.fn();
+    useAuthModule.useAuth.mockImplementation(() => ({
+      user: { _id: "123" },
+    }));
 
-    render(
-      <MemoryRouter>
-        <AuthProvider>
-          <LessonPlanCreate triggerFeedback={mockTriggerFeedback} />
-        </AuthProvider>
-      </MemoryRouter>
-    );
+    render(<LessonPlanCreate triggerFeedback={mockTriggerFeedback} />);
 
-    expect(await screen.findByLabelText("back-button")).toBeInTheDocument();
+    expect(await screen.findByLabelText("topic-input")).toBeInTheDocument();
+    expect(await screen.findByLabelText("save-button")).toBeInTheDocument();
+  });
+
+  it("renders a different page when an invalid user tries to create a lesson plan", async () => {
+    const mockTriggerFeedback = vi.fn();
+    useAuthModule.useAuth.mockImplementation(() => ({
+      user: { _id: "456" },
+    }));
+
+    render(<LessonPlanCreate triggerFeedback={mockTriggerFeedback} />);
+
+    expect(await screen.findByLabelText("title")).toBeInTheDocument();
+    expect(await screen.findByLabelText("message")).toBeInTheDocument();
   });
 });
