bb54cce45b0968acec9488753aa2840122b4bbf3
Rustic-Citrus <mrcurtis.english@gmail.com>
2024-07-09 12:39:55 +0100

    refactor: useAuth hook

--- src/hooks/useAuth.jsx
+++ src/hooks/useAuth.jsx
@@ -1,76 +1,13 @@
+import { useAuthHook } from "./useAuthHook";
+
 import { createContext, useContext, useMemo } from "react";
-import { useNavigate } from "react-router-dom";
-import { useLocalStorage } from "./useLocalStorage";
+
 import PropTypes from "prop-types";
-import UserService from "../service/UserService.js";
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [user, setUser] = useLocalStorage("user", null);
-  const navigate = useNavigate();
-  const { VITE_API_ENDPOINT } = import.meta.env;
-
-  const api = new UserService(VITE_API_ENDPOINT);
-
-  const authenticate = async () => {
-    try {
-      const response = await api.authenticate();
-
-      if (response.status >= 300) {
-        setUser(null);
-      }
-
-      return response;
-    } catch (error) {
-      console.log(error.message);
-    }
-  };
-
-  const signIn = async (userData) => {
-    try {
-      const response = await api.login(userData);
-
-      if (response.status === 200 && response.data.user._id) {
-        setTimeout(() => {
-          setUser(response.data.user);
-        }, 2000);
-      }
-
-      return response;
-    } catch (error) {
-      console.log(error.message);
-    }
-  };
-
-  const signOut = async () => {
-    try {
-      const response = await api.logout();
-
-      setUser(null);
-      navigate("/");
-
-      return response;
-    } catch (error) {
-      console.log(error.message);
-    }
-  };
-
-  const register = async (userData) => {
-    try {
-      const response = await api.register(userData);
-
-      if (response.status === 201) {
-        setTimeout(() => {
-          navigate("/login");
-        }, 3000);
-      }
-
-      return response;
-    } catch (error) {
-      console.log(error.message);
-    }
-  };
+  const [user, authenticate, signIn, signOut, register] = useAuthHook();
 
   const value = useMemo(
     () => ({
@@ -80,7 +17,7 @@ export const AuthProvider = ({ children }) => {
       register,
       authenticate,
     }),
-    [user]
+    [user, signIn, signOut, register, authenticate]
   );
 
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
---
+++ src/hooks/useAuthHook.jsx
@@ -0,0 +1,73 @@
+import { useLocalStorage } from "./useLocalStorage";
+import UserService from "../service/UserService.js";
+
+import { useNavigate } from "react-router-dom";
+
+export const useAuthHook = () => {
+  const [user, setUser] = useLocalStorage("user", null);
+  const navigate = useNavigate();
+  const { VITE_API_ENDPOINT } = import.meta.env;
+
+  const api = new UserService(VITE_API_ENDPOINT);
+
+  const authenticate = async () => {
+    try {
+      const response = await api.authenticate();
+
+      if (response.status >= 300) {
+        setUser(null);
+      }
+
+      return response;
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  const signIn = async (userData) => {
+    try {
+      const response = await api.login(userData);
+
+      if (response.status === 200 && response.data.user._id) {
+        setTimeout(() => {
+          setUser(response.data.user);
+        }, 2000);
+      }
+
+      return response;
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  const signOut = async () => {
+    try {
+      const response = await api.logout();
+
+      setUser(null);
+      navigate("/");
+
+      return response;
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  const register = async (userData) => {
+    try {
+      const response = await api.register(userData);
+
+      if (response.status === 201) {
+        setTimeout(() => {
+          navigate("/login");
+        }, 3000);
+      }
+
+      return response;
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  return [user, authenticate, signIn, signOut, register];
+};
