98c614b28b644b57dc28d7f1cab1f26e4feab9c1
Rustic-Citrus <mrcurtis.english@gmail.com>
2024-07-09 12:39:00 +0100

    chore: updated vite config

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,20 @@
-import { coverageConfigDefaults, defineConfig } from 'vitest/config'
+import { coverageConfigDefaults, defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/simplifica/",
+  base: "/simplifica",
   test: {
     environment: "jsdom",
     coverage: {
       reporter: ["text", "json", "html"],
-      exclude: ["src/main.jsx", "src/App.jsx", ...coverageConfigDefaults.exclude]
+      exclude: [
+        "src/main.jsx",
+        "src/App.jsx",
+        ...coverageConfigDefaults.exclude,
+      ],
     },
     globals: true,
-  }
+  },
 });
